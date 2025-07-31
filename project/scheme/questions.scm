f016d0836b09a0a4b4c31a5a5fb61781601e2c9f
AbyssSkb <abyssskb@outlook.com>
2025-07-31 10:53:42 +0800

    feat(scheme): complete all problems

--- project/scheme/questions.scm
+++ project/scheme/questions.scm
@@ -1,72 +1,93 @@
 (define (caar x) (car (car x)))
+
 (define (cadr x) (car (cdr x)))
+
 (define (cdar x) (cdr (car x)))
+
 (define (cddr x) (cdr (cdr x)))
 
-;; Problem 15
-;; Returns a list of two-element lists
+; ; Problem 15
+; ; Returns a list of two-element lists
 (define (enumerate s)
   ; BEGIN PROBLEM 15
-  'replace-this-line
-  )
-  ; END PROBLEM 15
+  (define (func i s)
+    (if (null? s)
+        nil
+        (cons (list i (car s)) (func (+ i 1) (cdr s)))))
+  (func 0 s))
 
-;; Problem 16
-
-;; Merge two lists S1 and S2 according to ORDERED? and return
-;; the merged lists.
+; END PROBLEM 15
+; ; Problem 16
+; ; Merge two lists S1 and S2 according to ORDERED? and return
+; ; the merged lists.
 (define (merge ordered? s1 s2)
   ; BEGIN PROBLEM 16
-  'replace-this-line
-  )
-  ; END PROBLEM 16
+  (cond 
+    ((null? s1)
+     s2)
+    ((null? s2)
+     s1)
+    ((ordered? (car s1) (car s2))
+     (cons (car s1) (merge ordered? (cdr s1) s2)))
+    (else
+     (cons (car s2) (merge ordered? s1 (cdr s2))))))
 
-;; Optional Problem 2
-
-;; Returns a function that checks if an expression is the special form FORM
+; END PROBLEM 16
+; ; Optional Problem 2
+; ; Returns a function that checks if an expression is the special form FORM
 (define (check-special form)
   (lambda (expr) (equal? form (car expr))))
 
 (define lambda? (check-special 'lambda))
-(define define? (check-special 'define))
-(define quoted? (check-special 'quote))
-(define let?    (check-special 'let))
 
-;; Converts all let special forms in EXPR into equivalent forms using lambda
+(define define? (check-special 'define))
+
+(define quoted? (check-special 'quote))
+
+(define let? (check-special 'let))
+
+; ; Converts all let special forms in EXPR into equivalent forms using lambda
 (define (let-to-lambda expr)
-  (cond ((atom? expr)
-         ; BEGIN OPTIONAL PROBLEM 2
-         'replace-this-line
-         ; END OPTIONAL PROBLEM 2
-         )
-        ((quoted? expr)
-         ; BEGIN OPTIONAL PROBLEM 2
-         'replace-this-line
-         ; END OPTIONAL PROBLEM 2
-         )
-        ((or (lambda? expr)
-             (define? expr))
-         (let ((form   (car expr))
-               (params (cadr expr))
-               (body   (cddr expr)))
-           ; BEGIN OPTIONAL PROBLEM 2
-           'replace-this-line
-           ; END OPTIONAL PROBLEM 2
-           ))
-        ((let? expr)
-         (let ((values (cadr expr))
-               (body   (cddr expr)))
-           ; BEGIN OPTIONAL PROBLEM 2
-           'replace-this-line
-           ; END OPTIONAL PROBLEM 2
-           ))
-        (else
-         ; BEGIN OPTIONAL PROBLEM 2
-         'replace-this-line
-         ; END OPTIONAL PROBLEM 2
-         )))
+  (cond 
+    ((atom? expr)
+     ; BEGIN OPTIONAL PROBLEM 2
+     expr
+     ; END OPTIONAL PROBLEM 2
+    )
+    ((quoted? expr)
+     ; BEGIN OPTIONAL PROBLEM 2
+     expr
+     ; END OPTIONAL PROBLEM 2
+    )
+    ((or (lambda? expr) (define? expr))
+     (let ((form (car expr))
+           (params (cadr expr))
+           (body (cddr expr)))
+       ; BEGIN OPTIONAL PROBLEM 2
+       (cons form (cons params (map let-to-lambda body)))
+       ; END OPTIONAL PROBLEM 2
+     ))
+    ((let? expr)
+     (let ((values (cadr expr))
+           (body (cddr expr)))
+       ; BEGIN OPTIONAL PROBLEM 2
+       (cons (cons `lambda
+                   (cons (car (zip values)) (map let-to-lambda body)))
+             (map let-to-lambda (cadr (zip values))))
+       ; END OPTIONAL PROBLEM 2
+     ))
+    (else
+     ; BEGIN OPTIONAL PROBLEM 2
+     (map let-to-lambda expr)
+     ; END OPTIONAL PROBLEM 2
+    )))
 
 ; Some utility functions that you may find useful to implement for let-to-lambda
-
 (define (zip pairs)
-  'replace-this-line)
+  (cond 
+    ((null? pairs)
+     (list nil nil))
+    ((null? (car pairs))
+     nil)
+    (else
+     (cons (map car pairs) (zip (map cdr pairs))))))
